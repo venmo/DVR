97f4e94f1b0103f2b748bdbc7cec1f0b96dadc50
Rui Peres <rui.peres@mailonline.co.uk>
2015-07-17 17:29:04 +0100

    Error with completion

--- DVR/SessionDataTask.swift
+++ DVR/SessionDataTask.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 class SessionDataTask: NSURLSessionDataTask {
-
+    
     // MARK: - Properties
-
+    
     weak var session: Session!
     let request: NSURLRequest
     let completion: ((NSData?, NSURLResponse?, NSError?) -> Void)?
-
-
+    
+    
     // MARK: - Initializers
-
+    
     init(session: Session, request: NSURLRequest, completion: ((NSData?, NSURLResponse?, NSError?) -> Void)? = nil) {
         self.session = session
         self.request = request
@@ -23,40 +23,50 @@ class SessionDataTask: NSURLSessionDataTask {
     override func cancel() {
         // Don't do anything
     }
-
+    
     override func resume() {
         let cassette = session.cassette
-
+        
         // Find interaction
         if let interaction = cassette?.interactionForRequest(request) {
-            // Forward completion
-            completion?(interaction.responseData, interaction.response, nil)
+            
+            // Check if it's within the valid status code range
+            let response = interaction.response as! NSHTTPURLResponse
+            if (self.validResponse(response)) {
+                completion?(interaction.responseData, response, nil)
+            }
+            else
+            {
+                let error = NSError(domain: (request.URL?.absoluteString)!, code: response.statusCode, userInfo: nil)
+                completion?(interaction.responseData, interaction.response, error)
+            }
+            
             return
         }
-
-		if cassette != nil {
-			fatalError("[DVR] Invalid request. The request was not found in the cassette.")
-		}
-
+        
+        if cassette != nil {
+            fatalError("[DVR] Invalid request. The request was not found in the cassette.")
+        }
+        
         // Cassette is missing. Record.
-		if session.recordingEnabled == false {
-			fatalError("[DVR] Recording is disabled.")
-		}
-
+        if session.recordingEnabled == false {
+            fatalError("[DVR] Recording is disabled.")
+        }
+        
         // Create directory
         let outputDirectory = session.outputDirectory.stringByExpandingTildeInPath
         let fileManager = NSFileManager.defaultManager()
         if !fileManager.fileExistsAtPath(outputDirectory) {
             try! fileManager.createDirectoryAtPath(outputDirectory, withIntermediateDirectories: true, attributes: nil)
         }
-
+        
         print("[DVR] Recording '\(session.cassetteName)'")
-
+        
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
             // Create cassette
             let interaction = Interaction(request: self.request, response: response!, responseData: data)
             let cassette = Cassette(name: self.session.cassetteName, interactions: [interaction])
-
+            
             // Persist
             do {
                 let outputPath = outputDirectory.stringByAppendingPathComponent(self.session.cassetteName).stringByAppendingPathExtension("json")!
@@ -66,9 +76,16 @@ class SessionDataTask: NSURLSessionDataTask {
             } catch {
                 // Do nothing
             }
-
-			fatalError("[DVR] Failed to persist cassette.")
+            
+            fatalError("[DVR] Failed to persist cassette.")
         }
         task?.resume()
+    }
+    
+    private func validResponse(response: NSHTTPURLResponse) -> Bool {
+        switch response.statusCode {
+        case 200..<300: return true
+        default : return false
+        }
     }
 }
