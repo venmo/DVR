1d402a40e2056995325819b00d11db96fb383d50
JP Wright <jp@contentful.com>
2017-09-29 11:45:47 +0200

    Update .travis.yml for Xcode 9 and add seperate build script
    Add swift build to the script

---
+++ Scripts/travis-build-test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -x -o pipefail
+
+if [[ "$SWIFT_BUILD" == "true" ]]; then
+    swift build
+    exit 0
+fi
+
+# -jobs -- specify the number of concurrent jobs
+# `sysctl -n hw.ncpu` -- fetch number of 'logical' cores in macOS machine
+xcodebuild -jobs `sysctl -n hw.ncpu` test -project DVR.xcodeproj -scheme ${TRAVIS_XCODE_SCHEME} -sdk ${TRAVIS_XCODE_SDK} \
+  -destination "platform=${DESTINATION}" ONLY_ACTIVE_ARCH=YES CODE_SIGNING_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+
