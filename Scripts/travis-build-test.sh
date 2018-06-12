8ca9a59c449ec8b458f0570d4239023fd61c0b23
JP Wright <jpwright31@gmail.com>
2018-06-12 18:28:22 +0200

    Use travis' built-in xcpretty

--- Scripts/travis-build-test.sh
+++ Scripts/travis-build-test.sh
@@ -10,5 +10,5 @@ fi
 # -jobs -- specify the number of concurrent jobs
 # `sysctl -n hw.ncpu` -- fetch number of 'logical' cores in macOS machine
 xcodebuild -jobs `sysctl -n hw.ncpu` test -project DVR.xcodeproj -scheme ${TRAVIS_XCODE_SCHEME} -sdk ${TRAVIS_XCODE_SDK} \
-  -destination "platform=${DESTINATION}" ONLY_ACTIVE_ARCH=YES CODE_SIGNING_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+  -destination "platform=${DESTINATION}" ONLY_ACTIVE_ARCH=YES CODE_SIGNING_IDENTITY="" CODE_SIGNING_REQUIRED=NO | xcpretty -c
 
