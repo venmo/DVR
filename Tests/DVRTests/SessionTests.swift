70cd45a1a72e24e66c16a5671577b488503710fd
m-herold <michael.herold@student.tugraz.at>
2019-09-23 20:00:28 +0200

    #93 Add tests for dataTask with URL

--- Tests/DVRTests/SessionTests.swift
+++ Tests/DVRTests/SessionTests.swift
@@ -41,6 +41,32 @@ class SessionTests: XCTestCase {
             XCTFail()
         }
     }
+    
+    func testDataTaskWithUrl() {
+        let url = URL(string: "http://example.com")!
+        let dataTask = session.dataTask(with: url)
+        
+        XCTAssert(dataTask is SessionDataTask)
+        
+        if let dataTask = dataTask as? SessionDataTask, let headers = dataTask.request.allHTTPHeaderFields {
+            XCTAssert(headers["testSessionHeader"] == "testSessionHeaderValue")
+        } else {
+            XCTFail()
+        }
+    }
+
+    func testDataTaskWithUrlAndCompletion() {
+        let url = URL(string: "http://example.com")!
+        let dataTask = session.dataTask(with: url, completionHandler: { _, _, _ in return })
+        
+        XCTAssert(dataTask is SessionDataTask)
+        
+        if let dataTask = dataTask as? SessionDataTask, let headers = dataTask.request.allHTTPHeaderFields {
+            XCTAssert(headers["testSessionHeader"] == "testSessionHeaderValue")
+        } else {
+            XCTFail()
+        }
+    }
 
     func testPlayback() {
         session.recordingEnabled = false
